98e29367a2da52288890156750d76ecf29a5e06a
CopiaPiratA <aleustrsa2004@gmail.com>
2024-06-02 20:36:54 -0300

    1.0

---
+++ src/App.tsx
@@ -0,0 +1,33 @@
+import { useState } from 'react'
+import './App.css'
+import MapComponent from './components/Map.tsx'
+
+function App() {
+  const [inputYear,setInputYear] = useState(0)
+  const [year,setYear] = useState(2023)
+  return (
+    <>
+      <h1>MontevideoDanger</h1>
+      <p><a href='https://catalogodatos.gub.uy/dataset/ministerio-del-interior-delitos_denunciados_en_el_uruguay' target='_blank'>Source</a></p>
+      <MapComponent year={year}/>
+      <input 
+      placeholder='2023'
+      type="number"
+      onChange={(e)=>{
+        setInputYear(parseInt(e.target.value, 10))
+      }} />
+      <button onClick={()=>{
+        if(inputYear >= 2013 && inputYear <= 2023) {
+          setYear(inputYear)
+        }
+        else {
+          alert('Only years betwin 2013 and 2023')
+          setYear(2023)
+        }
+      }} >Search</button>
+      <p>(No data for 2024)</p>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Map.tsx
@@ -0,0 +1,77 @@
+import React, { useEffect, useState } from "react";
+import { MapContainer, TileLayer, Polygon, Popup } from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
+
+interface Seccional {
+  seccional: string;
+  coords: [number, number][];
+  delitos: number;
+  year: string;
+}
+
+interface MapProps {
+  year: number;
+}
+
+const getColor = (delitos: number) => {
+  return delitos >= 22 ? '#8f0000' :
+         delitos >= 15 ? '#ed2a11' :
+         delitos >= 11 ? '#db5b0b' :
+         delitos >= 7 ? '#ed8e11' :
+         delitos >= 4 ? '#edba11' :
+         delitos >= 2  ? '#e8f25a':
+         delitos >= 0  ? '#57e334' :
+                        '#FFEDA0';
+}
+
+const MapComponent: React.FC<MapProps> = ({ year }) => {
+  const [seccionales, setSeccionales] = useState<Seccional[]>([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const requests = Array.from({ length: 25 }, (_, i) => i + 1).map(async (i) => {
+          const response = await fetch(`/api/SECCIONAL ${i}/${year}`);
+          if (!response.ok) {
+            console.error(`Failed to fetch data for SECCIONAL ${i}`);
+            return null; 
+          }
+          return response.json();
+        });
+
+        const results: (Seccional | null)[] = await Promise.all(requests);
+        const filteredResults: Seccional[] = results.filter((result): result is Seccional => result !== null);
+        setSeccionales(filteredResults);
+      } catch (error: any) {
+        console.error(error.message);
+      }
+    };
+
+    fetchData();
+  }, [year]);
+
+  return (
+    <MapContainer key={Math.random()} center={[-34.9011, -56.1645]} zoom={12} style={{height:"600px", width: '100%' }}>
+      <TileLayer
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      {seccionales.map(seccional => {
+        const delitosYear = seccional.delitos;
+        const color = getColor(delitosYear);
+        return (
+          <Polygon
+            key={Math.random()}
+            positions={seccional.coords}
+            pathOptions={{ color, weight: 2, fillOpacity: 0.7 }}
+          >
+            <Popup>
+             {seccional.delitos} Homicides In {year}
+            </Popup>
+          </Polygon>
+        );
+      })}
+    </MapContainer>
+  );
+}
+
+export default MapComponent;
