85c5cab0b394fe254e0d844d5f2d73973a11a193
CopiaPiratA <aleustrsa2004@gmail.com>
2024-06-04 00:13:50 -0300

    1.0

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,15 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react-swc';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Montevideo-Danger/',
-  build: {
-    outDir: 'dist'
-  },
   server: {
     proxy: {
-      '/api': 'http://localhost:3000'
+      '/api': {
+        target: 'http://localhost:3000',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '')
+      }
     }
   }
 });
