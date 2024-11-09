0e030fc0145257780bf20c962d306f151a9f8069
CopiaPiratA <aleustrsa2004@gmail.com>
2024-11-09 15:08:44 -0300

    removing node

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,13 +1,15 @@
-import React, { useEffect, useState } from "react";
 import { MapContainer, TileLayer, Polygon, Popup } from 'react-leaflet';
+import { useState, useEffect } from 'react';
+import { data } from "./data.ts";
 import 'leaflet/dist/leaflet.css';
 
 interface Seccional {
-  seccional: string;
+  nombre: string;
   coords: [number, number][];
-  delitos: number;
-  year: string;
+  delitos: { [year: string]: number }; 
 }
+
+type Seccionales = Seccional[];
 
 interface MapProps {
   year: number;
@@ -19,53 +21,32 @@ const getColor = (delitos: number) => {
          delitos >= 11 ? '#db5b0b' :
          delitos >= 7 ? '#ed8e11' :
          delitos >= 4 ? '#edba11' :
-         delitos >= 2  ? '#e8f25a':
-         delitos >= 0  ? '#57e334' :
+         delitos >= 2 ? '#e8f25a' :
+         delitos >= 0 ? '#57e334' :
                         '#FFEDA0';
 }
 
 const MapComponent: React.FC<MapProps> = ({ year }) => {
-  const [seccionales, setSeccionales] = useState<Seccional[]>([]);
+  const [seccionales, setSeccionales] = useState<Seccionales>([]);
 
   useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const requests = Array.from({ length: 25 }, (_, i) => i + 1).map(async (i) => {
-          const response = await fetch(`/api/SECCIONAL ${i}/${year}`);
-          if (!response.ok) {
-            console.error(`Failed to fetch data for SECCIONAL ${i}`);
-            return null; 
-          }
-          return response.json();
-        });
-
-        const results: (Seccional | null)[] = await Promise.all(requests);
-        const filteredResults: Seccional[] = results.filter((result): result is Seccional => result !== null);
-        setSeccionales(filteredResults);
-      } catch (error: any) {
-        console.error(error.message);
-      }
-    };
-
-    fetchData();
+   setSeccionales(data.seccionales); 
   }, [year]);
 
   return (
-    <MapContainer key={Math.random()} center={[-34.9011, -56.1645]} zoom={12} style={{height:"600px", width: '100%' }}>
-      <TileLayer
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
-      {seccionales.map(seccional => {
-        const delitosYear = seccional.delitos;
+    <MapContainer center={[-34.9011, -56.1645]} zoom={12} style={{ height: "600px", width: '100%' }}>
+      <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+      {seccionales.map((seccional, index) => {
+        const delitosYear = seccional.delitos[year.toString()] || 0;
         const color = getColor(delitosYear);
         return (
           <Polygon
-            key={Math.random()}
+            key={index} 
             positions={seccional.coords}
             pathOptions={{ color, weight: 2, fillOpacity: 0.7 }}
           >
             <Popup>
-             {seccional.delitos} Homicides In {year}
+              {delitosYear} Homicides In {year}
             </Popup>
           </Polygon>
         );
